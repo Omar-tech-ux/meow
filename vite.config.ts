276e009325b90182246e0c6abf81f1ece0bb1031
OmarSuleeq <sleeq1990@gmail.com>
2025-06-05 00:20:41 +0300

    Configure Vite For github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/meow/', // <-- set this to your repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+});
